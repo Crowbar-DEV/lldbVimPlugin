94d23556c06998192fa13a61c318432d3c37af4b
Gavin <gmcellistrem@gmail.com>
2024-02-26 16:13:39 -0600

    pls

--- rplugin/python3/plugin.py
+++ rplugin/python3/plugin.py
@@ -1,13 +1,10 @@
 import neovim
-import os
 
 @neovim.plugin
-class Plugin(object):
-    def __init__(self, nvim):
-        self.nvim = nvim
+class Main(object):
+    def __init__(self, vim):
+        self.vim = vim
 
-    @neovim.function('step_over')
-    def step_over(self, args):
-        os.system('python3 ~/shithole/CrowCave/vimtest/testsrc/lldbAdapter/client.py')
-
-
+    @neovim.function('DoItPython')
+    def doItPython(self, args):
+        self.vim.command('echo "hello from DoItPython"')
